08d0d01ec6ec007089c7ae8cb65724fbd7cd886b
dullsun <Lordshiva5@9>
2020-06-22 20:15:50 -0400

    fixed vulnerabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {CustomerModule} from './customer/customer.module';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        CustomerModule,
-        AppRoutingModule,
-    ],
+  imports: [
+    BrowserModule,
+    CustomerModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
